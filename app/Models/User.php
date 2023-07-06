675474f4fdd9a96519865fd566725d44578bb9d3
Vahn Gomes <Github@codycody.org>
2023-07-06 19:44:45 -0400

    Simplify department relationship in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,7 +53,7 @@ class User extends Authenticatable
      */
     public function department()
     {
-        return $this->belongsTo(Department::class, 'department_id');
+        return $this->belongsTo(Department::class);
     }
 
     /**
