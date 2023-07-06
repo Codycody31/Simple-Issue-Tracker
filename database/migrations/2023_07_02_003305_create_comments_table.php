4a9abe9637b9854a4bdfad2ab9fe8caa36e815a9
Vahn Gomes <Github@codycody.org>
2023-07-06 19:47:08 -0400

    Alter `comments` table schema

--- database/migrations/2023_07_02_003305_create_comments_table.php
+++ database/migrations/2023_07_02_003305_create_comments_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('comments', function (Blueprint $table) {
-            $table->integer('comment_id')->primary();
+            $table->id();
             $table->foreignId('issue_id')->constrained('issues');
             $table->text('comment');
             $table->foreignId('user_id')->constrained('users');
