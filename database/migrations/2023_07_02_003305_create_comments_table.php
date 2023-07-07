fe27f3a9e9652c214d2e3f211851c52e8900c00f
Vahn Gomes <Github@codycody.org>
2023-07-07 14:54:07 -0400

    Add cascade delete constraint to issue_id foreign key
    - Adds cascade delete constraint to the issue_id foreign key in the comments table's migration. This ensures that when an issue is deleted, all associated comments are also deleted automatically.

--- database/migrations/2023_07_02_003305_create_comments_table.php
+++ database/migrations/2023_07_02_003305_create_comments_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('issue_id')->constrained('issues');
+            $table->foreignId('issue_id')->constrained('issues')->onDelete('cascade');
             $table->text('comment');
             $table->foreignId('user_id')->constrained('users');
             $table->timestamps();
