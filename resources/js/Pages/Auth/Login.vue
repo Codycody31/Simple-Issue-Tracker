fc96ef99dfe3e38ba2b07f9855e43425a3b4ff46
Vahn Gomes <Github@codycody.org>
2023-07-09 16:42:33 -0400

    Update Login.vue UI and functionality.

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,28 +1,41 @@
-<script setup>
+<script>
 import Checkbox from "@/Components/Checkbox.vue";
 import GuestLayout from "@/Layouts/GuestLayout.vue";
 import InputError from "@/Components/InputError.vue";
 import { Head, Link, useForm } from "@inertiajs/vue3";
 
-defineProps({
-    canResetPassword: {
-        type: Boolean,
+export default {
+    components: {
+        Checkbox,
+        GuestLayout,
+        Head,
+        InputError,
+        Link,
     },
-    status: {
-        type: String,
+
+    props: {
+        canResetPassword: Boolean,
+        status: String,
     },
-});
 
-const form = useForm({
-    email: "",
-    password: "",
-    remember: false,
-});
+    setup(props) {
+        const form = useForm({
+            email: "",
+            password: "",
+            remember: false,
+        });
 
-const submit = () => {
-    form.post(route("login"), {
-        onFinish: () => form.reset("password"),
-    });
+        const submit = () => {
+            form.post(route("login"), {
+                onFinish: () => form.reset("password"),
+            });
+        };
+
+        return {
+            form,
+            submit,
+        };
+    },
 };
 </script>
 
@@ -30,100 +43,171 @@ const submit = () => {
     <GuestLayout>
         <Head title="Log in" />
 
-        <div class="h-100 d-flex jsutify-content-center align-items-center">
-            <div class="w-100">
-                <!-- Session Status -->
-                <div
-                    v-if="status"
-                    class="mb-4 font-medium text-sm text-green-600"
-                >
+        <div class="login-page">
+            <div class="login-form">
+                <h3 class="login-header">Issue Tracker System</h3>
+                <div class="alert alert-success" v-if="status">
                     {{ status }}
                 </div>
-
-                <!-- Login -->
-                <h3 class="py-5 text-center">Simple Issue Tracker System</h3>
-                <div class="card my-3 col-md-4 offset-md-4">
-                    <div class="card-body">
-                        <form @submit.prevent="submit" id="login-form">
-                            <center>
-                                <small>Please enter your credentials.</small>
-                            </center>
-
-                            <!-- Email Address -->
-                            <div class="form-group">
-                                <label for="email" class="control-label"
-                                    >Email</label
-                                >
-                                <input
-                                    id="email"
-                                    type="email"
-                                    class="form-control form-control-sm rounded-0"
-                                    v-model="form.email"
-                                    required
-                                    autofocus
-                                    autocomplete="username"
-                                />
-
-                                <InputError
-                                    class="mt-2"
-                                    :message="form.errors.email"
-                                />
-                            </div>
-
-                            <!-- Password -->
-                            <div class="form-group">
-                                <label for="password" class="control-label"
-                                    >Password</label
-                                >
-                                <input
-                                    type="password"
-                                    id="password"
-                                    name="password"
-                                    class="form-control form-control-sm rounded-0"
-                                    required
-                                    v-model="form.password"
-                                    autocomplete="current-password"
-                                />
-                                <InputError
-                                    class="mt-2"
-                                    :message="form.errors.password"
-                                />
-                            </div>
-
-                            <!-- Remember Me -->
-                            <div class="block mt-4">
-                                <label class="flex items-center">
-                                    <Checkbox
-                                        name="remember"
-                                        v-model:checked="form.remember"
-                                    />
-                                    <span class="ml-2 text-sm text-gray-600"
-                                        >Remember me</span
-                                    >
-                                </label>
-                            </div>
-
-                            <!-- Login -->
-                            <div class="flex items-center justify-end mt-4">
-                                <Link
-                                    v-if="canResetPassword"
-                                    :href="route('password.request')"
-                                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                                >
-                                    Forgot your password?
-                                </Link>
-                                <button
-                                    class="btn btn-sm btn-primary rounded-0 my-1 ml-4"
-                                    :class="{ 'opacity-25': form.processing }"
-                                    :disabled="form.processing"
-                                >
-                                    Login
-                                </button>
-                            </div>
-                        </form>
-                    </div>
+                <div class="text-center">
+                    <img
+                        src="@/assets/imgs/Simple-Issue-Tracker.png"
+                        alt="Issue Tracker"
+                        width="100"
+                        height="100"
+                        class="d-inline-block align-top"
+                    />
                 </div>
+                <form @submit.prevent="submit" id="login-form">
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input
+                            id="email"
+                            type="email"
+                            class="form-control"
+                            v-model="form.email"
+                            required
+                            autofocus
+                            autocomplete="username"
+                        />
+                        <InputError
+                            class="error-message"
+                            :message="form.errors.email"
+                        />
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            class="form-control"
+                            required
+                            v-model="form.password"
+                            autocomplete="current-password"
+                        />
+                        <InputError
+                            class="error-message"
+                            :message="form.errors.password"
+                        />
+                    </div>
+
+                    <div class="remember-me">
+                        <label class="checkbox-label">
+                            <Checkbox
+                                name="remember"
+                                v-model:checked="form.remember"
+                            />
+                            <span class="ml-2">Remember me</span>
+                        </label>
+                    </div>
+
+                    <div class="forgot-password">
+                        <Link
+                            v-if="canResetPassword"
+                            :href="route('password.request')"
+                            class="forgot-password-link"
+                        >
+                            Forgot your password?
+                        </Link>
+                    </div>
+
+                    <button
+                        class="login-button"
+                        :class="{ 'opacity-25': form.processing }"
+                        :disabled="form.processing"
+                    >
+                        Login
+                    </button>
+                </form>
             </div>
         </div>
     </GuestLayout>
 </template>
+
+<style scoped>
+.login-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #f5f5f5;
+}
+
+.login-form {
+    width: 400px;
+    padding: 40px;
+    background-color: #fff;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.login-header {
+    text-align: center;
+    margin-bottom: 20px;
+    color: #333;
+}
+
+.form-group {
+    margin-bottom: 20px;
+}
+
+.form-group label {
+    display: block;
+    margin-bottom: 5px;
+    font-weight: 500;
+    color: #333;
+}
+
+.form-control {
+    width: 100%;
+    height: 40px;
+    padding: 0 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    background-color: #fff;
+    font-size: 14px;
+    color: #333;
+}
+
+.remember-me {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 10px;
+}
+
+.checkbox-label {
+    display: flex;
+    align-items: center;
+    color: #333;
+}
+
+.forgot-password {
+    text-align: right;
+    margin-bottom: 10px;
+}
+
+.forgot-password-link {
+    color: #333;
+}
+
+.login-button {
+    display: block;
+    width: 100%;
+    height: 40px;
+    padding: 0 10px;
+    border: none;
+    border-radius: 5px;
+    background-color: #3b82f6;
+    color: #fff;
+    font-size: 14px;
+    cursor: pointer;
+}
+
+.disabled {
+    opacity: 0.5;
+    cursor: not-allowed;
+}
+</style>
