83bbb35ca6773720ef069ef45b8befe4306cc419
Vahn Gomes <Github@codycody.org>
2023-07-09 18:26:40 -0400

    feat: Add formatDate and sortIssuesByDate methods

--- resources/js/Pages/Issues/Index.vue
+++ resources/js/Pages/Issues/Index.vue
@@ -86,6 +86,38 @@ export default {
             handleIssueSubmit,
         };
     },
+    methods: {
+        /**
+         * Format date to locale string
+         * @param {*} date Date to be formatted
+         * @returns Formatted date
+         */
+        formatDate(date) {
+            if (!date) {
+                return "";
+            }
+            try {
+                return new Date(date).toLocaleString();
+            } catch (error) {
+                console.error(error);
+                return "";
+            }
+        },
+        /**
+         * Sort issues by date
+         * @param {*} issues Issues to be sorted
+         * @returns Sorted issues
+         */
+        sortIssuesByDate(issues) {
+            if (issues && Array.isArray(issues)) {
+                return issues.sort((a, b) => {
+                    return new Date(b.created_at) - new Date(a.created_at);
+                });
+            } else {
+                throw new Error("Invalid issues array");
+            }
+        },
+    },
 };
 </script>
 
@@ -104,7 +136,11 @@ export default {
     </div>
     <hr />
     <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
-        <div class="col" v-for="issue in issues" :key="issue.id">
+        <div
+            class="col"
+            v-for="issue in sortIssuesByDate(issues)"
+            :key="issue.id"
+        >
             <div
                 class="w-100 bg-dark text-light bg-gradient opcaity-70 py-3 px-2 rounded-lg shadow-sm"
             >
@@ -144,6 +180,12 @@ export default {
                         >Open</span
                     >
                     <span class="w-auto badge bg-danger" v-else>Closed</span>
+
+                    <!-- Timestamp -->
+                    <span class="w-auto badge bg-secondary">
+                        {{ formatDate(issue.created_at) }}
+                    </span>
+
                     <Link
                         class="btn btn-sm btn-primary col-auto py-0 rounded-lg"
                         :href="route('issues.show', issue.id)"
