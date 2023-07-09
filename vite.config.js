fb20667ccbef36048fb487d35fbab156b989cb72
Vahn Gomes <Github@codycody.org>
2023-07-09 19:05:05 -0400

    feat: Add PWA configuration for Issue Tracker, in beta

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,16 @@ export default defineConfig({
                     includeAbsolute: false,
                 },
             },
+            pwa: {
+                name: 'Issue Tracker',
+                themeColor: '#4DBA87',
+                msTileColor: '#000000',
+                appleMobileWebAppCapable: 'yes',
+                appleMobileWebAppStatusBarStyle: 'black',
+
+                // configure the workbox plugin
+                workboxPluginMode: 'GenerateSW',
+            }
         }),
     ],
 });
