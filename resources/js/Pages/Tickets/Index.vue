bda598790999ada59ac42f3d778fdaa31d55efec
Vahn Gomes <Github@codycody.org>
2023-07-10 18:17:52 -0400

    Fixed incorrectly spelled css class name

--- resources/js/Pages/Tickets/Index.vue
+++ resources/js/Pages/Tickets/Index.vue
@@ -62,7 +62,7 @@ export default {
         <small>Below are the list of issues posted into your department</small>
     </div>
     <hr />
-    <div class="ow row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
+    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
         <div
             class="col"
             v-for="ticket in tickets"
