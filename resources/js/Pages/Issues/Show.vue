896b063464d648f494e50ba3363ede2b192ece39
Vahn Gomes <me@vahngomes.dev>
2023-08-06 08:43:44 -0700

    Refactor delete comment buttons in Show.vue

--- resources/js/Pages/Issues/Show.vue
+++ resources/js/Pages/Issues/Show.vue
@@ -446,25 +446,26 @@ export default {
                 Are you sure you want to delete this comment?
             </p>
             <hr class="mt-4" />
-            <div class="flex justify-center mt-4">
+            <!-- Submit -->
+            <div class="mt-6 text-center flex justify-between">
                 <button
                     class="inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest bg-gray-600 hover:bg-gray-700 focus:outline-none focus:border-gray-700 focus:ring-gray-500 disabled:opacity-50"
                     @click="closeModal">
                     Cancel
                 </button>
+
                 <button
-                    class="inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest bg-red-600 hover:bg-red-700 focus:outline-none focus:border-red-700 focus:ring-red-500 disabled:opacity-50 ml-2"
+                    class="inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest bg-red-600 hover:bg-red-700 focus:outline-none focus:border-red-700 focus:ring-red-500 disabled:opacity-50"
                     :disabled="deleteCommentForm.processing" @click="handleDeleteCommentSubmit()">
                     <svg xmlns="http://www.w3.org/2000/svg" class="mr-2" width="32" height="32" viewBox="0 0 24 24">
                         <path fill="currentColor"
                             d="M19 6.5l-1.5-1.5-5.5 5.5-5.5-5.5L5 6.5l5.5 5.5-5.5 5.5L6.5 19l5.5-5.5 5.5 5.5L19 17.5l-5.5-5.5Z" />
                     </svg>
+
                     <span v-if="deleteCommentForm.processing">
-                        Deleting...
+                        Processing...
                     </span>
-                    <span v-else>
-                        Delete
-                    </span>
+                    <span v-else>Delete </span>
                 </button>
             </div>
         </div>
