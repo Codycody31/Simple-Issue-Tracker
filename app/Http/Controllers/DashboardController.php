b71b5231b362eff6c131b14749e1bf744fd0faa3
Vahn Gomes <me@vahngomes.dev>
2023-08-06 08:58:39 -0700

    feat: Add user, departments, and issues to the Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -18,6 +18,31 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return Inertia::render('Dashboard');
+        // Get user
+        $user = auth()->user();
+
+        // Get all departments
+        $departments = Department::all();
+
+        // If user is admin, get all issues
+        if ($user->type === 1) {
+            $issues = Issue::limit(5)
+                ->get()
+                ->load('user', 'department');
+        }
+        // If user is staff, get issues of the department
+        else {
+            $issues = Issue::where('user_id', $user->id)
+                ->orWhere('department_id', $user->department_id)
+                ->limit(5)
+                ->get()
+                ->load('user', 'department');
+        }
+
+        // Return dashboard
+        return Inertia::render('Dashboard', [
+            'departments' => $departments,
+            'issues' => $issues,
+        ]);
     }
 }
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -8,13 +8,18 @@ export default {
         Head,
     },
     layout: AuthenticatedLayout,
+    props: {
+        departments: Array,
+        issues: Array,
+        users: Array,
+    },
 };
 </script>
 
 <template>
     <Head title="Dashboard" />
 
-    <div class="container">
+    <div class="container mt-4">
         <!-- Welcome -->
         <div class="row">
             <div class="col-md-12">
@@ -22,5 +27,41 @@ export default {
                 <hr />
             </div>
         </div>
+
+        <!-- Issue Tracker Content -->
+        <div class="row">
+            <div class="col-md-6">
+                <!-- Department Section -->
+                <div class="card">
+                    <div class="card-header bg-primary text-white">
+                        <h4>Departments</h4>
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-group">
+                            <li class="list-group-item" v-for="department in departments" :key="department.id">
+                                {{ department.name }}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <!-- Issues Section -->
+                <div class="card">
+                    <div class="card-header bg-danger text-white">
+                        <h4>Issues</h4>
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-group">
+                            <li class="list-group-item" v-for="issue in issues" :key="issue.id">
+                                {{ issue.title }}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
+  
